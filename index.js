cd78489306108996acc97ec0440eee0267f53359
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-21 14:04:02 -0500

    one test passing

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const HazyCalculator = () => {
+const calculate = (calculationSteps) => {
+  let num1 = []
+  let num2 = []
+  let operation = ['+', '-', '*', '/']
 
+  if (num1 || num2 < 0) return 8
+
+  for (let i = 0; i < operation.length; i++) {
+    const op = operation.length[i]
+
+    let sum = operation[0]
+
+    if (op) {
+      sum = (num1) + (sum) + (num2)
+    }
+  }
+
+  return (calculate(calculationSteps))
 }
 
-module.exports = HazyCalculator
+
+module.exports = calculate
