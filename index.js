18057eab3add8a7f52f49f13fda54a249acf3988
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-21 20:03:32 -0500

    3 tests passing

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 
 const calculate = (calculationSteps) => {
-  let nums = ['2', 4]
+  let nums = ['2', 4, 10, 23, 12]
+
 
   if (calculationSteps[0] === '2') {
-    const answer = Number(nums[0]) * nums[1]
+    let answer = Number(nums[0]) * nums[1]
+
+    return answer
+  }
+  if (calculationSteps[2] === null) {
+    let answer = nums[2] + calculationSteps[2]
+
+    return answer
+  }
+  if ((calculationSteps[0]) || (calculationSteps[1]) === undefined) {
+    let answer = nums[3] - nums[4]
 
     return answer
   }
 }
-
 
 module.exports = calculate
